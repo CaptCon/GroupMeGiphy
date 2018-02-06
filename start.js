a46ee6c4e3fb5e91e085bb162b84d02bb0b644a1
CaptCon <36173888+CaptCon@users.noreply.github.com>
2018-02-06 15:04:25 -0600

    Update start.js

--- start.js
+++ start.js
@@ -40,7 +40,7 @@ app.listen(port, () => {
 
 app.post("/", (req, res) => {
     postText = "";
-    const toSearchFor = "@giphy ";
+    const toSearchFor = "!gif ";
     const index = req.body.text.toLowerCase().indexOf(toSearchFor);
     
     //If we found the string we're looking for get results and send them
